6bb6ea48f3e6c0268f21eed1acc1d3841adcb40d
Felipe <f.rodriguez06@ufromail.cl>
2017-06-01 13:16:38 -0400

    Index incompleto

--- index.php
+++ index.php
@@ -1,9 +1,39 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+    <link rel="stylesheet" href="css/materialize.scss">
+    <link rel="stylesheet" href="css/Estilo.css">
+    <script type="text/javascript" src="js/jquery-3.1.1.js"></script>
+	<title>Venta Buses Chile</title>
 </head>
 <body>
+    <div id="container">
 
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo right">Logo</a>
+                <ul id="nav-mobile" class="left hide-on-med-and-down">
+                    <li><a href="sass.html">Sass</a></li>
+                    <li><a href="badges.html">Components</a></li>
+                    <li><a href="collapsible.html">JavaScript</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <div id="formularioBusqueda" class="mdl-grid">
+            <div class="mdl-cell mdl-cell--1-col">
+                <form>
+                    <label>Buscar por:</label>
+                    <select>
+                        <option value="1">Año</option>
+                        <option value="2">Carroceria</option>
+                        <option value="3">Marca</option>
+                        <option value="4">Ciudad</option>
+                    </select>
+                </form>
+            </div>
+
+        </div>
+    </div>
 </body>
 </html>
